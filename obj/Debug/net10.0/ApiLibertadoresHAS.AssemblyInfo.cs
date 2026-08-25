49b905e725d62929bcfb1f17a867fde87de08b62
KauaMHAS <blood47750@gmail.com>
2026-08-24 21:47:04 -0300

    Conexao com Azure

--- obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a98e62e22fb6870b34c3f699192f3672fb7e40a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4128b2109d7ad0494726d47867b3e362c3e262")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
